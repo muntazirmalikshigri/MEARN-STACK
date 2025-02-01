c87a838b88b3c477d6c7e1396b9d6230a3a73f1d
muntazirmalikshigri <muntazirshigri.12@gmail.com>
2025-02-02 03:53:09 +0500

    conditions in js

---
+++ javscript/class_02/app.js
@@ -0,0 +1,17 @@
+//   operators
+let a=4;
+let b=10
+// arithmatic oprator
+let sum = a+b;
+console.log(sum);
+// assignment operator
+let c = b;
+console.log(c);
+// uniry oprerator
+let d = a++;
+// camparison operator
+let comp=a==b;
+console.log(comp)
+// logical operator
+let logi=b && a;
+console.log(logi)
---
+++ javscript/class_03/script.js
@@ -0,0 +1,36 @@
+// if conditions
+
+let color = "red";
+if(color=="red"){
+    console.log("stop")
+}if(color=="green"){
+    console.log("google")
+}if(color=="yellow"){
+    console.log("ready")
+}
+
+
+// if else condition
+
+let size = "L";
+if(size =="XL"){
+    console.log(`The popcorn price ${250}`)
+}else if(size=="L"){
+    console.log(`price is ${100}`)
+}else if(size=="M"){
+    console.log(`The popcorn price is ${200}`)
+}  
+
+
+// nested condition
+
+let marks =50;
+if(marks>=33){
+    if(marks>=80){
+        console.log("o")
+    }else{
+        console.log("A")
+    }
+}else{
+    console.log("Fail")
+}
