ee332059ef68d622abbde51494dab4dcd1100099
muntazirmalikshigri <muntazirshigri.12@gmail.com>
2025-02-05 05:38:05 +0500

    for loop and while loop

---
+++ javscript/class_08/app.js
@@ -0,0 +1,48 @@
+// //      loops
+
+// //  print odd numbers
+
+// for(let i =1; i<=15 ;i=i+2){
+//     console.log(i)
+// }
+
+// //    backwards loop
+// console.log("Backwards");
+// for(let i =15;i>=1;i=i-2){
+//     console.log(i)
+// }
+
+// // print even numbers
+// console.log("Even numbers")
+// for(let i= 2; i<=10;i=i+2){
+//     console.log(i)
+// }
+
+// // backwards
+// console.log("Backwards")
+// for(let i= 10; i>=2;i=i-2){
+//     console.log(i)
+// }
+
+
+// //   multipalications
+// console.log("table using loop for loop")
+// for(let i=5;i<=30;i=i+5){
+//     console.log(i)
+// }
+
+// let n= +prompt("Enter your number");
+// for(let i=n;i<=n*10;i=i+n){
+//     console.log(i)
+// }
+
+for(let i = 1;i<=3;i++){
+    console.log("outer")
+    for(let j=1 ; j<=5;j++){
+      
+        console.log(j);
+    }
+}
+
+
+
