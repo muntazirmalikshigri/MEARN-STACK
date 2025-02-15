e71f2e74c6b5d8c8c615c90c4030fd4511efb99a
muntazirmalikshigri <muntazirshigri.12@gmail.com>
2025-02-16 04:19:35 +0500

    dom start

---
+++ javscript/DOM/app.js
@@ -0,0 +1,7 @@
+let id=document.getElementById("malik").style.color="red";
+function myFunc(){
+    const para = document.getElementsByTagName('h1')[0];
+    para.innerHTML = "I Love My MOM &hearts;";
+    para.style.color="magenta";
+    console.log(id);
+};
